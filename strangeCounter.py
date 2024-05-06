8e2448144c3fb9639fcf5e7b07285c0b88648d9c
ovie <oimara@enprohsoft.com>
2024-05-06 09:31:01 +0100

    strangeCounter.py

--- strangeCounter.py
+++ strangeCounter.py
@@ -1,29 +1,28 @@
-
 def strangeCounter(t):
+    # Write your code here
     i = cycle(t)
     value = i + 2
 
-    while True:
-        if i == t:
-            break
+    return value - (t - i)
 
-        i += 1
-        value -= 1
+    # return binaryCycle(t, i, i + value - 1, value, 1)
 
-    return value
 
-    # time, value = cycle(t)
-    # end = time + value
-    # t1 = time
-    #
-    # while t1 < end:
-    #     if t == t1:
-    #         break
-    #
-    #     t1 += 1
-    #     value -= 1
-    #
-    # return value
+def binaryCycle(t, start, end, start_value, end_value):
+    mid = (start + end) // 2
+    mid_value = start_value - (mid - start)
+
+    if mid == t:
+        return mid_value
+
+    elif mid > t:
+        end = mid - 1
+        return binaryCycle(t, start, end, start_value, mid_value + 1)
+
+    elif mid < t:
+        start = mid + 1
+        return binaryCycle(t, start, end, mid_value - 1, end_value)
+
 
 def cycle(t):
     i = 1
@@ -41,76 +40,6 @@ def cycle(t):
         m *= 2
 
     return i
-
-
-# def strangeCounter(t):
-#     start, end = cycle(t)
-#     value = start + 2
-#     t1 = start
-#
-#     while t1 <= end:
-#         if t == t1:
-#             break
-#
-#         t1 += 1
-#         value -= 1
-#
-#     return value
-
-# def cycle(t):
-#     num = 1
-#     factor = 3
-#
-#     while True:
-#         if t in range(num, num+factor):
-#             break
-#
-#         num += factor
-#         factor *= 2
-#
-#     return num, factor
-
-# def cycle(t):
-#     t1 = 1
-#     cycle = 0
-#     start = 0
-#     end = 0
-#
-#     while True:
-#         v = t1 + 2
-#         t2 = t1 + v
-#         cycle += 1
-#         if t1 <= t < t2:
-#             start = t1
-#             end = t2 - 1
-#             break
-#
-#         t1 = t2
-#     return start, end
-
-# def cycle(t):
-#     t1 = 1
-#     cycle = 0
-#     start = 0
-#     end = 0
-#
-#     while True:
-#         v = t1 + 2
-#         t2 = t1 + v
-#         cycle += 1
-#         if t1 <= t < t2:
-#             start = t1
-#             end = t2 - 1
-#             break
-#
-#         t1 = t2
-#     return start, end
-
-t = 4
-# t = 5
-# t = 9
-# t = 10
-# t = 21
 
 
 t = 22
